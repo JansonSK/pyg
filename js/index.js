8085b5badfb9debc9701e27c863d74b76d5a87f8
xujinsheng <1156551536@qq.com>
2019-04-03 20:00:04 +0800

    轮播图的写成

--- js/index.js
+++ js/index.js
@@ -1,22 +1,29 @@
 $(function () {
 
     $.ajax({
-      type:'get',
-      url:'http://157.122.54.189:9094/api/public/v1/home/swiperdata',
-      dataType:'jaon',
-      success:function(result){
-          console.log(result)
-          if(result.meta.status==200){
-            var html = template('lbt_banner',result)
-            console.log(html)
-            $('.banner').html(html)
-          }
-      }
+        type: 'get',
+        url: 'http://157.122.54.189:9094/api/public/v1/home/swiperdata',
+        dataType: 'json',
+        success: function (result) {
+            console.log(result)
+
+
+            if (result.meta.status == 200) {
+                var html = template('lbt_banner', result)
+                console.log(html)
+                $('.indexBanner').html(html)
+
+                var indexHtml = template('lbt_selreot', result)
+                $('.mui-slider-indicator').html(indexHtml)
+                //  控制轮播图的自动切换的速度
+                mui('.mui-slider').slider({
+                    interval: 1000 //自动轮播周期，若为0则不自动播放，默认为0；
+                });
+            }
+        }
 
     })
 
 
-    mui('.mui-slider').slider({
-        interval: 2000 //自动轮播周期，若为0则不自动播放，默认为0；
-    });
+
 })
